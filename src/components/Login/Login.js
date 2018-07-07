4ccf681d15b10bc80076f352c6b1ed2a12935011
Xuehui Chen <jeffreychen2016@gmail.com>
2018-07-07 09:45:00 -0500

    set up components

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+class Login extends React.Component {
+  render () {
+    return (
+      <div className="login">Login</div>
+    );
+  };
+};
+
+export default Login;
