fd465b278f3ff8acac63bb6e814b5da26e7b0a33
Xuehui Chen <jeffreychen2016@gmail.com>
2018-07-09 20:51:05 -0500

    added get request for OrderSpa component

--- src/components/OrderSpa/OrderSpa.js
+++ src/components/OrderSpa/OrderSpa.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './OrderSpa.css';
+import orderRequests from '../../firebaseRequests/orders';
+import authRequests from '../../firebaseRequests/auth';
 
 class OrderSpa extends React.Component {
+  state = {
+    orders: [],
+  };
+
+  componentDidMount () {
+    orderRequests
+      .getRequest(authRequests.getUid())
+      .then((orders) => {
+        // this.setState({orders : orders});
+        this.setState({ orders });
+      })
+      .catch((err) => {
+        console.error('error with order get request', err);
+      });
+  };
+
   render () {
+    const orderComponents = this.state.orders.map((order) => {
+      return <button>{order.id}</button>;
+    });
     return (
-      <div className="OrderSpa">OrderSpa</div>
+      <div className="OrderSpa">
+        <h2>Orders</h2>
+        <button>New Order</button>
+        <ul>
+          {orderComponents}
+        </ul>
+      </div>
     );
   };
 };
