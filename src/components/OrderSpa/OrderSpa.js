a679741a3b17fb096c5cede5f4dd2c11c869da63
Xuehui Chen <jeffreychen2016@gmail.com>
2018-07-09 21:01:40 -0500

    added Link to /new

--- src/components/OrderSpa/OrderSpa.js
+++ src/components/OrderSpa/OrderSpa.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './OrderSpa.css';
 import orderRequests from '../../firebaseRequests/orders';
 import authRequests from '../../firebaseRequests/auth';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 class OrderSpa extends React.Component {
   state = {
@@ -22,12 +24,21 @@ class OrderSpa extends React.Component {
 
   render () {
     const orderComponents = this.state.orders.map((order) => {
-      return <button>{order.id}</button>;
+      return (
+        <button
+          key={order.id}
+          className="col-xs-12 btn btn-default order-button"
+        >
+          <span className="col-xs-6">Order Number: {order.id}</span>
+          <span className="col-xs-6">Order Date: {moment(order.dateTime).format('LLL')}</span>
+        </button>);
     });
     return (
-      <div className="OrderSpa">
+      <div className="OrderSpa col-xs-12">
         <h2>Orders</h2>
-        <button>New Order</button>
+        <button>
+          <Link to="/new">New Order</Link>
+        </button>
         <ul>
           {orderComponents}
         </ul>
