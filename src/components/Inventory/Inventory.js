2f8cfbd6ad0e504b611b4929d7429a8856f12527
Xuehui Chen <jeffreychen2016@gmail.com>
2018-07-09 19:26:00 -0500

    added get request for inventory component

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import './Inventory.css';
+import fishRequests from '../../firebaseRequests/fishes';
 
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  };
+
+  componentDidMount () {
+    fishRequests
+      .getRequest()
+      .then((fishes) => {
+        // this.setState({fishes : fishes});
+        this.setState({fishes});
+      })
+      .catch((err) => {
+        console.error('error with fish get request', err);
+      });
+  };
+
   render () {
+    const fishComponents = this.state.fishes.map((fish) => {
+      return (
+        <h2>{fish.name}</h2>
+      );
+    });
+
     return (
-      <div className="Inventory">Inventory</div>
+      <div className="Inventory">
+        <h1>Inventory</h1>
+        <ul className='fishes'>
+          {fishComponents}
+        </ul>
+      </div>
     );
   };
 };
